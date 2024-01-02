c4edf04e2529dd7bb091591963b3ee1d74acfb9a
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-02 14:07:56 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log("2" > 1);
+// console.log("2" == 1);
+// console.log("2" <=1);
+
+//normal comparisions can compare between different datatypes  
+
+
+// console.log("2" === 2); // strict check
+// double equal to is used to compare the aperands and their datatypes
+
+
+// console.log(null > 0);    //false
+// console.log(null == 0);     //false
+// console.log(null >= 0);     //true
+
+//this is because the comparision operator converts the value of null
+
+// like wise the indefined will give an output as false for all the comparisions
+
+console.log(undefined > 0);    //false
+console.log(undefined == 0);     //false
+console.log(undefined >= 0);     //false
+
+// in js the comparions operator `==` and {<, >, <=, >=] works differently aand these are different
+
+
