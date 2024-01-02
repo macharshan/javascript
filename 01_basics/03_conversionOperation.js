f364e5a1f124ac8ab41d0c9b989ef8b89ee9376a
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-02 09:39:18 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33" 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let newNumber = Number(score);
+// console.log(typeof newNumber);
+
+
+// `33` => 33
+// `33abc` => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false 
+// "" => false
+// "harsha" => true
+
+let ranNum = 55
+
+let numberInString = String(ranNum);
+console.log(numberInString);
+console.log(typeof numberInString);
