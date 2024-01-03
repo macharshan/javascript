be53e3d67f8d6c17568a87254ed93071661aae4a
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-03 16:26:53 +0000

    datatype_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,33 @@
+// Primitive datatype
+
+// 7 Types: String, Number,  Boolean, null, undefined, Symbol, Bigint
+
+const myString = `macharshan`;
+const myNumber = 123456;
+let isLoggedIn = false;
+let temp = null;
+let userMail;
+let mySymbol = Symbol(`123`);
+
+console.table([myString, myNumber, isLoggedIn, temp, userMail, mySymbol]);
+
+
+// Non-Primitive (Reference type)
+// array, object, function
+
+let myarray = [`harsha`, `vardhan`, `macharla`];
+
+let myObj = {
+    firstName: `Harsha`,
+    middleName: `vardhan`,
+    lastName: `macharla`
+}
+
+let myFunction = function(){
+    console.log(`hello world`);
+}
+
+
+console.table([myarray, myObj]);
+
+console.log(myFunction);
