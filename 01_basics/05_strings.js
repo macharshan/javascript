49d6c93ecf0df4338f64cd3bc0028efb43c929e8
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-04 11:07:33 +0000

    string conpleted till split

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -19,16 +19,31 @@ const newName = new String(`harshaVardhan`)
 
 
 const subStr = newName.substring(0, 5);
-console.log(subStr);
+// console.log(subStr);
 
 // substring uses only positive values 
 
 const strSlice = newName.slice(-13, 5);
-console.log(strSlice);
+// console.log(strSlice);
 
 // str slice can take negative values also
 
 const anotherName = `                      Macharla Harsha Vardhan     `
-console.log(anotherName.trim());
+// console.log(anotherName.trim());
 
+// trims the ends of a string
 
+const fullName = `Harsha Vardhan Macharla`
+// console.log(fullName.replaceAll(` `, `_`, 2));
+
+// .replace will change only the begining value of the string 
+// but .replaceAll will change all the occurances in a string
+
+// we can also ask a question whether the string consists the value with .includes object 
+// but it is case sensitive 
+
+// console.log(fullName.includes(`Harsha`));
+
+// we can alsi concert a string to a array form by using split
+
+console.log(fullName.split(` `));
