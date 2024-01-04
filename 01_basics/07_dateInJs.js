5eee4eebe85e6b83c1fe490f467d6cce0f94d2a6
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-04 17:46:57 +0000

    dateInJs

---
+++ 01_basics/07_dateInJs.js
@@ -0,0 +1,27 @@
+
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let createdDate = new Date(2024, 0, 1)
+// let createdDate = new Date(2024, 0, 1, 18, 30)
+// let createdDate = new Date(`2024-06-29`)
+let createdDate = new Date(`2025-06-29`)
+// console.log(createdDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console;log(createdDate.getTime())
+
+// console.log(Date.now())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(myDate)
+console.log(myDate.getMonth() +1)
+console.log(myDate.getDay())
