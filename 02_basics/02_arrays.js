fbd010053525e8e06c314a8379cb15149bf29218
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-05 15:08:27 +0000

    flat method

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -18,14 +18,19 @@ let all_bros2 = [...meternal_bros, ...peternal_bros]
 
 // console.log(all_bros2)
 
+let numb = [1, 2, 3, [4, 5, 6], 4, 5, [5, 6, 7, [3, 4, 5, 6]]];
+
+let newarr = numb.flat(Infinity)
+console.log(newarr);
+
 
 // their are also aother methods to convert and ask the arrays 
 
-console.log(Array.isArray(`harsha`));
-console.log(Array.from(`harsha`));
+// console.log(Array.isArray(`harsha`));
+// console.log(Array.from(`harsha`));
 
 let harsh_age = 21
 let alekhya_age = 24
 let vaish_age = 24
 
-console.log(Array.of(harsh_age, alekhya_age, vaish_age));
+// console.log(Array.of(harsh_age, alekhya_age, vaish_age));
