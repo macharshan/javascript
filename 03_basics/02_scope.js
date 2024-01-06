e63fedd49d61d169e164ed214b93fde266b0e769
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-06 08:46:51 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,61 @@
+{
+
+}
+
+if (true) {
+    // let a= 10
+    // const b = 20
+    var c = 30
+}
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+function one() {
+    const name = `harsha`
+    function two() {
+        email = `harsha@gmaildotcom`
+        // console.log(`this is ${name} and my email id is ${email}`);
+    }
+    // console.log(email)
+
+
+    two()
+
+
+}
+
+
+one()
+
+
+if (true) {
+    const name = `harsha`
+    if(1){
+        const mail = `hello@gmail.com`
+        // console.log(name)
+    }
+    // console.log(mail)
+    
+}
+
+// console.log(name)
+
+
+addone(5)
+
+function addone(num){
+    console.log(num+1)
+}
+
+
+// addtwo(5) //cannot be executed as the variable is declared after calling the function
+
+const addtwo = function(num){
+    console.log(num+1)
+}
+
+
