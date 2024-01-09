d7ba7de3c014f512fc01482561054a6ff22cc5f2
macharshan <117523543+macharshan@users.noreply.github.com>
2024-01-09 14:25:00 +0000

    while_loop

---
+++ 05_loops(iterations)/02_while_doWhile.js
@@ -0,0 +1,55 @@
+// while (condition) {
+    
+// }
+
+let i = 1
+while (i<=20){
+    if(i == 5){
+        // console.log(`found num 5`);
+        break;
+    }
+    // console.log(i);
+    i++
+}
+
+let number = 0
+while (number <= 7){
+    if(number == 5){
+        // console.log(`found num 5`);
+        number++
+        continue;
+    }
+    // console.log(number);
+    number++
+}
+
+
+// let myArray = [`harsha` , `vardhan`, `macharla`]
+
+// let index=0
+// while(index < myArray.length){
+//     // console.log(`the element is ${myArray[index]}`);
+//     index++
+// }
+
+// let num=11
+// do{
+//     // console.log(num);
+//     num++
+// }while(i<=10)
+
+
+// let i = 0;
+// let n = 0;
+
+// while (i < 5) {
+//   i++;
+
+//   if (i === 3) {
+//     console.log(`found 3`);
+//     continue;
+//   }
+
+//   n += i;
+//   console.log(n);
+// }
