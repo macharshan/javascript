145eb206e531b32e65f26ee8def8f33d76d7e938
macharshan <harshavardhan2906@gmail.com>
2024-01-14 02:06:12 +0530

    bmi added range

--- 07_projects/02_BMI_calculator/index.js
+++ 07_projects/02_BMI_calculator/index.js
@@ -11,8 +11,17 @@ addEventListener('submit',function(e){
     } else if (weightValue == `` || weightValue <= 0 || isNaN(weightValue)){
         result.innerText = `please add a valid weight`;
     } else{
-        const bmi = (weightValue/((heightValue*heightValue)/10000
-        ))
-        result.innerText = `your BMI value is ${bmi.toFixed(2)}`;
+        const bmi = (weightValue/((heightValue*heightValue)/10000));
+        let range =``
+        if(bmi < 18.5){
+            range = `Underweight`
+        } else if(bmi >= 18.5 && bmi <= 24.9){
+            range =`Normal weight`
+        } else if(bmi >= 25 && bmi <= 29.9){
+            range = `OverWeight`
+        } else{
+            range = `Obesity`
+        }
+        result.innerText = `your BMI value is ${bmi.toFixed(2)} (${range})`;
     }
 })
